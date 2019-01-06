4ed9b0480a6e6f2e2ce17c3bc44698f8212553c8
mekapiku <mekapiku@gmail.com>
2019-01-06 19:55:19 +0900

    idの付与が間違っていたので修正

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ templace_unlock_path = "template/unlock.png"
 threshold = 0.99
 mag_size = int(sys.argv[3])
 
-
 # source image
 screenshot = cv2.imread(source_path)
 img_gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
@@ -34,19 +33,38 @@ result_unlock = cv2.matchTemplate(img_gray, template_unlock, cv2.TM_CCOEFF_NORME
 loc_lock = np.where(result_lock >= threshold)
 loc_unlock = np.where(result_unlock >= threshold)
 
-# output object
-leafee = {"leafee" : []}
+# reformat result to hash
+leafee_list = []
 
+# locked objects
+loc_x = loc_lock[0]; loc_y = loc_lock[1]
+index = 0
+while index < len(loc_x):
+    leafee_list.append({"x":loc_x[index], "y":loc_y[index], "state":"locked"})
+    index += 1
+
+# unlocked objects
+loc_x = loc_unlock[0]; loc_y = loc_unlock[1]
+index = 0
+while index < len(loc_x):
+    leafee_list.append({"x":loc_x[index], "y":loc_y[index], "state":"unlocked"})
+    index += 1
+
+# Sort By x -> y
+# example: id1 -> 0, 0, id2 -> 100, 0
+#          id3 -> 0, 100, ...
+sorted_leafee_list = sorted(
+  leafee_list,
+  key = lambda x: (x["y"], x["x"])
+)
+
+# output leafee
+leafee = {}; leafee["leafee"] = []
+
+# reformat result hash to json
 count = 0
-
-# result lock
-for pt in zip(*loc_lock[::-1]):
-    leafee["leafee"].append({"id" : count, "state" : "locked"})
-    count += 1
-
-# result unlock
-for pt in zip(*loc_unlock[::-1]):
-    leafee["leafee"].append({"id" : count, "state" : "unlocked"})
+for item in sorted_leafee_list:
+    leafee["leafee"].append({"id" : count, "state" : item["state"]})
     count += 1
 
 # TODO unkown state
@@ -55,5 +73,5 @@ while count < mag_size:
     count += 1
 
 # Output
-out = open(output_path, 'w')
-json.dump(leafee, out, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+with open(output_path, 'w') as out:
+    json.dump(leafee, out, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
